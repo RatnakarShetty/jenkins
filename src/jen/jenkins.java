6a97404f76f94f796ebd302e47b728f0835c797c
tyss <tyss@localhost>
2017-11-15 15:05:31 +0530

    updated now 3:05

--- src/jen/jenkins.java
+++ src/jen/jenkins.java
@@ -1,14 +1,32 @@
 package jen;
 
 import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class jenkins {
 	
-	@Test
-	public void test1(){
-		System.out.println("Test1 jenkins");
+	@BeforeMethod(alwaysRun=true)
+	public void Login(){
+		Reporter.log("Login",true);
+	}
+	
+	@AfterMethod(alwaysRun=true)
+	public void LogOut(){
+		Reporter.log("Loginout",true);
+	}
+	
+	@Test(dependsOnMethods={"action2"})
+	public void action1(){
+		Reporter.log("action1",true);
+	}
+	
+	@Test()
+	public void action2(){
+		Reporter.log("action2",true);
 		Assert.fail();
 	}
-
+	
 }
